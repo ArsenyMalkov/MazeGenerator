9dfec405f0300d9ba77b49a29a8f9f1b0367dd43
Arseny Malkov <arsenymalkov@gmail.com>
2019-03-17 09:16:36 +0800

    Custom width & height

--- maze_generator.go
+++ maze_generator.go
@@ -11,48 +11,74 @@ type cell struct {
 }
 
 func main() {
-	//width := 5
-	//height := 5
+	width := 3
+	height := 3
 
-	var maze [5 * 5]cell
+	maze := make([]cell, width*height)
 
-	for y := 0; y < 5; y++ {
-		for x := 0; x < 5; x++ {
-			maze[y*5+x].x = x
-			maze[y*5+x].y = y
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			maze[y*width+x].x = x
+			maze[y*width+x].y = y
 		}
 	}
 
 	var stack []*cell
 
 	rand.Seed(time.Now().UnixNano())
-	curCell := &maze[rand.Intn(5*5)]
+	//r := rand.Intn(width * height)
+	r := 1
+	curCell := &maze[r]
+	fmt.Println(r)
 	stack = append(stack, curCell)
-	unvisited := 5*5 - 1
+	unvisited := width*height - 1
 
-	for unvisited != 0 {
+	for unvisited > -1 {
+		prtWidth, prtHeight := 2*width-1, 2*height-1
+		prtMaze := make([]string, prtWidth*prtHeight)
+		for y := 0; y < prtHeight; y++ {
+			for x := 0; x < prtWidth; x++ {
+				prtMaze[y*prtWidth+x] = "*"
+			}
+		}
+		for y := 0; y < height; y++ {
+			for x := 0; x < width; x++ {
+				prtMaze[y*2*prtWidth+x*2] = "-"
+				for _, curCell := range maze[y*width+x].connected {
+					dx, dy := curCell.x-x, curCell.y-y
+					prtMaze[(y*2+dy)*prtWidth+x*2+dx] = "+"
+				}
+			}
+		}
 
-		if curCell.x-1 != -1 && len(maze[curCell.y*5+curCell.x-1].connected) == 0 {
-			curCell.connected = append(maze[curCell.y*5+curCell.x].connected, &maze[curCell.y*5+curCell.x-1])
-			curCell = &maze[curCell.y*5+curCell.x-1]
+		for y := 0; y < prtHeight; y++ {
+			for x := 0; x < prtWidth; x++ {
+				fmt.Print(prtMaze[y*prtWidth+x])
+			}
+			fmt.Print("\n")
+		}
+		fmt.Println("\n", curCell.x, curCell.y)
+		if curCell.x-1 != -1 && len(maze[curCell.y*width+curCell.x-1].connected) == 0 {
+			curCell.connected = append(maze[curCell.y*width+curCell.x].connected, &maze[curCell.y*width+curCell.x-1])
+			curCell = &maze[curCell.y*width+curCell.x-1]
 			stack = append(stack, curCell)
 			unvisited -= 1
 			continue
-		} else if curCell.x+1 != 5 && len(maze[curCell.y*5+curCell.x+1].connected) == 0 {
-			curCell.connected = append(maze[curCell.y*5+curCell.x].connected, &maze[curCell.y*5+curCell.x+1])
-			curCell = &maze[curCell.y*5+curCell.x+1]
+		} else if curCell.x+1 != width && len(maze[curCell.y*width+curCell.x+1].connected) == 0 {
+			curCell.connected = append(maze[curCell.y*width+curCell.x].connected, &maze[curCell.y*width+curCell.x+1])
+			curCell = &maze[curCell.y*width+curCell.x+1]
 			stack = append(stack, curCell)
 			unvisited -= 1
 			continue
-		} else if curCell.y-1 != -1 && len(maze[(curCell.y-1)*5+curCell.x].connected) == 0 {
-			curCell.connected = append(maze[curCell.y*5+curCell.x].connected, &maze[(curCell.y-1)*5+curCell.x])
-			curCell = &maze[(curCell.y-1)*5+curCell.x]
+		} else if curCell.y-1 != -1 && len(maze[(curCell.y-1)*width+curCell.x].connected) == 0 {
+			curCell.connected = append(maze[curCell.y*width+curCell.x].connected, &maze[(curCell.y-1)*width+curCell.x])
+			curCell = &maze[(curCell.y-1)*width+curCell.x]
 			stack = append(stack, curCell)
 			unvisited -= 1
 			continue
-		} else if curCell.y+1 != 5 && len(maze[(curCell.y+1)*5+curCell.x].connected) == 0 {
-			curCell.connected = append(maze[curCell.y*5+curCell.x].connected, &maze[(curCell.y+1)*5+curCell.x])
-			curCell = &maze[(curCell.y+1)*5+curCell.x]
+		} else if curCell.y+1 != width && len(maze[(curCell.y+1)*width+curCell.x].connected) == 0 {
+			curCell.connected = append(maze[curCell.y*width+curCell.x].connected, &maze[(curCell.y+1)*width+curCell.x])
+			curCell = &maze[(curCell.y+1)*width+curCell.x]
 			stack = append(stack, curCell)
 			unvisited -= 1
 			continue
@@ -61,25 +87,26 @@ func main() {
 		curCell, stack = stack[len(stack)-1], stack[:len(stack)-1]
 	}
 
-	var prtMaze [(5 + 4) * (5 + 4)]string
-	for y := 0; y < 9; y++ {
-		for x := 0; x < 9; x++ {
-			prtMaze[y*9+x] = "*"
+	prtWidth, prtHeight := 2*width-1, 2*height-1
+	prtMaze := make([]string, prtWidth*prtHeight)
+	for y := 0; y < prtHeight; y++ {
+		for x := 0; x < prtWidth; x++ {
+			prtMaze[y*prtWidth+x] = "*"
 		}
 	}
-	for y := 0; y < 5; y++ {
-		for x := 0; x < 5; x++ {
-			prtMaze[y*2*9+x*2] = "-"
-			for _, curCell := range maze[y*5+x].connected {
-				wx, wy := curCell.x-x, curCell.y-y
-				prtMaze[(y*2+wy)*9+x*2+wx] = "+"
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			prtMaze[y*2*prtWidth+x*2] = "-"
+			for _, curCell := range maze[y*width+x].connected {
+				dx, dy := curCell.x-x, curCell.y-y
+				prtMaze[(y*2+dy)*prtWidth+x*2+dx] = "+"
 			}
 		}
 	}
 
-	for y := 0; y < 9; y++ {
-		for x := 0; x < 9; x++ {
-			fmt.Print(prtMaze[y*9+x])
+	for y := 0; y < prtHeight; y++ {
+		for x := 0; x < prtWidth; x++ {
+			fmt.Print(prtMaze[y*prtWidth+x])
 		}
 		fmt.Print("\n")
 	}
